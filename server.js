32194d55a57a5b8c383acc8ccbf1fc52df24b96f
ZahraDH28 <diahandokozahra29@gmail.com>
2025-11-29 23:17:48 +0700

    test

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const cors = require("cors");
 const mapRoutes = require("./routes/mapRoutes");
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "https://zahradh28.github.io/Frontend_TitikBanjir/",
+    "https://vercel.com/zahradh28s-projects/frontend-titik-banjir",
+    "http://localhost:3000"
+  ],
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Route GeoJSON
 app.use("/api/map", mapRoutes);
 
- // Jalankan server
- const PORT = 4000;
- app.listen(PORT, () => console.log(`Backend running at https://vercel.com/zahradh28s-projects/frontend-titik-banjir`));
+// Jalankan server
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Backend running at http://localhost:${PORT}`));
